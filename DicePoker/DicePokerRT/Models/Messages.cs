e68c1200ebe235d704090d3bf8489e5f0eb9a015
Anton Makarevich <anton.makarevich@gmail.com>
2013-01-04 03:21:42 +0300

    Get user name functionality

--- DicePoker/DicePokerRT/Models/Messages.cs
+++ DicePoker/DicePokerRT/Models/Messages.cs
@@ -11,9 +11,12 @@ namespace Sanet.Kniffel.Models
         public const string NETWORK_GAME_IS_NOT_READY= "NetworkGameNotReadyMessage";
         public const string NEW_GAME_START = "StartNewGameMessage";
         public const string NEW_GAME_PLAYERS = "PlayersLabel";
+        public const string NEW_GAME_ADD_HUMAN = "AddPlayerLabel";
+        public const string NEW_GAME_ADD_BOT = "AddBotLabel";
 
         public const string PLAYER_NAME = "PlayerNameLabel";
         public const string PLAYER_PASSWORD = "PlayerPasswordLabel";
         public const string PLAYER_TYPE = "PlayerTypeLabel";
+        public const string PLAYER_NAME_DEFAULT = "PlayerNameDefault";
     }
 }
--- DicePoker/DicePokerRT/ViewModels/NewGameViewModel.cs
+++ DicePoker/DicePokerRT/ViewModels/NewGameViewModel.cs
@@ -8,6 +8,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System.UserProfile;
 
 namespace Sanet.Kniffel.ViewModels
 {
@@ -16,27 +17,7 @@ namespace Sanet.Kniffel.ViewModels
         #region Constructor
         public NewGameViewModel()
         {
-            Players = new ObservableCollection<Player>();
-            //trying toload previous players from roaming
-            for (int i = 0; i < 4; i++)
-            {
-                var p=RoamingSettings.GetLastPlayer(i);
-                if (p == null)
-                    break;
-                Players.Add(p);
-            }
-            //if no players loaded - add one default
-            if (!HasPlayers)
-                for (int i = 1; i < 5; i++)
-                {
-                    Players.Add(new Player()
-                    {
-                        Name = "Player "+i.ToString(),
-                        Type = PlayerType.Local
-                    });
-                }
-               
-            NotifyPropertyChanged("Players");
+            fillPlayers();
         }
         #endregion
 
@@ -60,6 +41,26 @@ namespace Sanet.Kniffel.ViewModels
             get
             {
                 return Messages.NEW_GAME_PLAYERS.Localize();
+            }
+        }
+        /// <summary>
+        /// Add Player appbar buton caption
+        /// </summary>
+        public string AddPlayerLabel
+        {
+            get
+            {
+                return Messages.NEW_GAME_ADD_HUMAN.Localize();
+            }
+        }
+        /// <summary>
+        /// Add Bot appbar buton caption
+        /// </summary>
+        public string AddBotLabel
+        {
+            get
+            {
+                return Messages.NEW_GAME_ADD_BOT.Localize();
             }
         }
 
@@ -91,6 +92,54 @@ namespace Sanet.Kniffel.ViewModels
                     return true;
                 return false;
             }
+        }
+        /// <summary>
+        /// If players count less then 4 we can add one more player
+        /// </summary>
+        public bool CanAddPlayer
+        {
+            get
+            {
+                if (Players != null && Players.Count <4)
+                    return true;
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// fills players list
+        /// </summary>
+        async void  fillPlayers()
+        {
+            Players = new ObservableCollection<Player>();
+            //trying toload previous players from roaming
+            for (int i = 0; i < 4; i++)
+            {
+                var p=RoamingSettings.GetLastPlayer(i);
+                if (p == null)
+                    break;
+                Players.Add(p);
+            }
+            //if no players loaded - add one default
+            if (!HasPlayers)
+            {
+                //get username from system
+                string userName = await UserInformation.GetDisplayNameAsync();
+                if (string.IsNullOrEmpty(userName))
+                    userName = await UserInformation.GetFirstNameAsync() + await UserInformation.GetFirstNameAsync();
+                //if no luck - add default name
+                if (string.IsNullOrEmpty(userName))
+                    userName = Messages.PLAYER_NAME_DEFAULT.Localize();
+                Players.Add(new Player()
+                    {
+                        Name = userName,
+                        Type = PlayerType.Local
+                    });
+            }  
+            NotifyPropertyChanged("Players");
         }
         #endregion
     }
