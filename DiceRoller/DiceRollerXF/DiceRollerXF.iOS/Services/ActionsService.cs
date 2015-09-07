ee1a1b1ba49d362c7ef5e0a704967fd8785eccfe
Anton Makarevich <anton.makarevich@gmail.com>
2015-09-07 09:22:24 +0300

    ios platform actions

--- DiceRoller/DiceRollerXF/DiceRollerXF.iOS/Services/ActionsService.cs
+++ DiceRoller/DiceRollerXF/DiceRollerXF.iOS/Services/ActionsService.cs
@@ -1,8 +1,11 @@
+using Foundation;
+using MessageUI;
 using Sanet.Kniffel.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UIKit;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(ActionsService))]
@@ -12,22 +15,40 @@ namespace Sanet.Kniffel.Services
     {
         public void NavigateToUrl(string url)
         {
-            //MainActivity.Instance.NavigateToUrl(url);
+            UIApplication.SharedApplication.OpenUrl(new NSUrl(url));
         }
 
         public void RateApp(string appName)
         {
-            //MainActivity.Instance.RateApp(appName);
+            if (UIDevice.CurrentDevice.CheckSystemVersion(7, 0))
+                UIApplication.SharedApplication.OpenUrl(new NSUrl("itms-apps://itunes.apple.com/app/id826095442"));
+            else
+                UIApplication.SharedApplication.OpenUrl(new NSUrl("itms-apps://itunes.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?type=Purple+Software&id=826095442"));
         }
-
+        
         public void SendEmail(string to, string topic, string body)
         {
-            //MainActivity.Instance.SendEmail(to, topic, body);
+            try
+            {
+                var _emailComposerView = new MFMailComposeViewController();
+                _emailComposerView.SetSubject("Sanet Dice (iOS)");
+                _emailComposerView.SetToRecipients(new string[] { to });
+
+                _emailComposerView.Finished += (sender, e) =>
+                {
+                    e.Controller.InvokeOnMainThread(() =>
+                    {
+                        e.Controller.DismissViewController(true, null);
+                    });
+                };
+                UIApplication.SharedApplication.KeyWindow.RootViewController.PresentViewController(_emailComposerView, true, null);
+            }
+            catch { }
         }
 
         public void ShowOtherApps(string parametr)
         {
-            //MainActivity.Instance.ShowMyApps(parametr);
+            UIApplication.SharedApplication.OpenUrl(new NSUrl("https://itunes.apple.com/us/artist/anton+makarevich/id826095445?ign-mpt=uo%3D4"));
         }
     }
 }
